41bc50a087eb9daee40d5147341c233bee24047e
Siria Mazzocchi <167107766+mazzocchi7053622@users.noreply.github.com>
2024-06-17 16:09:16 +0200

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
+    path('members/<int:pk>/profile/', views.ShowProfilePageView.as_view(), name='show_profile_page'),
     path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
     path('create_profile_page/', CreateProfilePAgeView.as_view(), name='create_profile_page'),
 ]
