c196198c3740e6eae82c8662fc3e5188af465426
Siria Mazzocchi <167107766+mazzocchi7053622@users.noreply.github.com>
2024-06-17 17:20:03 +0200

    Update views.py

--- members/views.py
+++ members/views.py
@@ -32,8 +32,6 @@ class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
 
-    def get_object(self, queryset=None):
-        return self.model.objects.get(pk=self.kwargs['pk'])
 
     def get_context_data(self, *args, **kwargs):
         context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
