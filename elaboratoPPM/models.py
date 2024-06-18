cbc15ba7ce1f1b8209c5eae1af78279a9f8508ea
Siria Mazzocchi <167107766+mazzocchi7053622@users.noreply.github.com>
2024-06-18 17:36:35 +0200

    Update models.py

--- elaboratoPPM/models.py
+++ elaboratoPPM/models.py
@@ -58,7 +58,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
