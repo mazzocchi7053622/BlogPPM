f67ce3453b22604fa81e7dfc1cc6516b3c00e999
Siria Mazzocchi <167107766+mazzocchi7053622@users.noreply.github.com>
2024-06-18 17:45:09 +0200

    Update models.py

--- elaboratoPPM/models.py
+++ elaboratoPPM/models.py
@@ -63,4 +63,4 @@ class Comment(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '%s - %s' % (self.post.title, self.name)
+        return '%s - %s' % (self.post.title, self.author)
